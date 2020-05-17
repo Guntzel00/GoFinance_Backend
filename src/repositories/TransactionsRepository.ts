df22f6e8887cd64ffaaade207b0245bde67ff552
Gustavo Guntzel <gustavo.g.s01@hotmail.com>
2020-05-17 00:51:59 -0700

    Missing fix bug with transaction creation

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,49 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+    const incomeArray: Array<number> = [];
+    const outcomeArray: Array<number> = [];
+
+    if (transactions.length > 0) {
+      transactions.forEach((transaction): void => {
+        const { type, value } = transaction;
+
+        if (type === 'income') {
+          incomeArray.push(value);
+        } else {
+          outcomeArray.push(value);
+        }
+      });
+
+      console.log(incomeArray);
+      console.log(outcomeArray);
+    }
+
+    // const test = incomeArray.reduce(
+    //   (currentValue: number, previousValue: number) => {
+    //     return currentValue + previousValue;
+    //   },
+    // );
+
+    const income: number = incomeArray.reduce(
+      (accumulator: number, currentValue: number): number => {
+        console.log(accumulator, currentValue);
+        return accumulator + currentValue;
+      },
+      0,
+    );
+    const outcome: number = outcomeArray.reduce(
+      (accumulator: number, currentValue: number): number => {
+        return accumulator + currentValue;
+      },
+      0,
+    );
+    return {
+      income,
+      outcome,
+      total: income - outcome,
+    };
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,61 @@
-// import AppError from '../errors/AppError';
-
+import { getCustomRepository, getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  categoryTitle: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    categoryTitle,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoriesRepository = getRepository(Category);
+    const balance = await transactionsRepository.getBalance();
+
+    if (balance.total < value && type === 'outcome') {
+      throw new AppError(
+        'Insufficient balance on account, transaction not completed',
+      );
+    }
+
+    let checkCategoryExists = await categoriesRepository.findOne({
+      where: { title: categoryTitle },
+    });
+
+    if (!checkCategoryExists) {
+      const newCategory = categoriesRepository.create({
+        title,
+      });
+
+      checkCategoryExists = newCategory;
+
+      await categoriesRepository.save(newCategory);
+    }
+
+    // const category = await categoriesRepository.findOne({
+    //   where: { title: categoryTitle },
+    // });
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id: checkCategoryExists.id,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
